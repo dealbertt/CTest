6336199af3120215be383d90c7e1d9e45b84b941
dealbertt <dealbertignacio@gmail.com>
2025-07-04 16:29:47 +0200

    Small improvement

--- include/ctest.h
+++ include/ctest.h
@@ -1,9 +1,30 @@
 #ifndef CTEST_H
 #define CTEST_H
 #include <stdbool.h>
-bool assertEqual(int expected, int (*funcPointer)(int, int), int a, int b);
+#include <assert.h>
+#include <string.h>
+#define MAX_TESTS 20
+typedef struct{
+    void (*testName)(void);
+    char name[20];
+}CTest;
 
-int sumTest(int a, int b);
+typedef struct{;
+    char name[20];
+    CTest test[MAX_TESTS];
+    bool testOccupied[MAX_TESTS];
+}TestGroup;
+
+void test_add(void);
+int initGroup(TestGroup *group);
+int addTest(TestGroup *group, const char *name, CTest *test);
+int runTests(TestGroup *group);
+
+
+bool assertEqualTest(int expected, int (*funcPointer)(int count, ...), int count, ...);
+bool assertEqualInt(int expected, int (*funcPointer)(int, int), int a, int b);
+bool assertEqualDouble(double expected, int (*funcPointer)(double, double), double a, double b);
+
 
 
 #endif
--- src/ctest.c
+++ src/ctest.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
 #include "../include/ctest.h"
 
-int sum(int a, int b){
+int sumTest(int a, int b){
     return a + b;
 }
 
-bool assertEqual(int expected, int (*funcPointer)(int, int), int a, int b){
+bool assertEqualInt(int expected, int (*funcPointer)(int, int), int a, int b){
     int result = funcPointer(a, b);
     if(expected == result){
         printf("Yay it works, equal!\n");
@@ -16,6 +18,53 @@ bool assertEqual(int expected, int (*funcPointer)(int, int), int a, int b){
     }
 }
 
-int main(){
-    assertEqual(4, sumTest, 3, 2);
+
+void test_add(void){
+    printf("Hola que tal\n");
 }
+bool assertEqualTest(int expected, int (*funcPointer)(int count, ...), int count, ...){
+    va_list list;
+    va_start(list, count);
+
+    return false;
+}
+
+int initGroup(TestGroup *group){
+    for(int i = 0; i < MAX_TESTS; i++){
+        group->testOccupied[i] = false;
+    }
+    return 0;
+}
+int addTest(TestGroup *group, const char *name, CTest *test){
+    for(int i = 0; i < MAX_TESTS; i++){
+        if(!group->testOccupied[i]){
+            group->test[i] = *test;
+            strncpy(group->test[i].name, name, sizeof(group->test[i].name) - 1);
+            group->test[i].name[sizeof(group->test[i].name) - 1] = '\0';
+            printf("Test %s added succesfully!\n", group->test[i].name);
+            group->testOccupied[i] = !group->testOccupied[i];
+            return 0;
+        }
+    }
+    return -1;
+}
+
+int runTests(TestGroup *group){
+    for(int i = 0; i < MAX_TESTS; i++){
+        if(group->testOccupied[i]){
+            printf("Running test: %s\n", group->test[i].name);
+            group->test[i].testName();
+        }
+    }
+    return 0;
+}
+
+int main(){
+    TestGroup group;
+    CTest test = {test_add};
+    addTest(&group, "First try", &test);
+    runTests(&group);
+
+    return 0;
+}
+
